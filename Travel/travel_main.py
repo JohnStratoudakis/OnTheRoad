0254061600e87924d5d689bf61fda03bcdd51d75
John Stratoudakis <john@wallstreetfpga.com>
2019-07-11 06:59:10 -0400

    More

---
+++ Travel/travel_main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.7
+
+def main() -> None:
+
+    print("MAIN")
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.7
+
+from setuptools import setup
+
+VERSION = "0.0.1"
+
+setup(
+    name="Travel",
+    version=VERSION,
+    author="John Stratoudakis",
+    author_email="johnstratoudakis@gmail.com",
+    license="",
+    packages=["Travel"],
+    entry_points={
+        "console_scripts": [
+            "travel=Travel.travel_main:main"
+            ]
+        },
+)
