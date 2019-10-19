132ffde3cd1ae3aca22be631115c81c9a693c9dc
John Stratoudakis <john@wallstreetfpga.com>
2019-10-19 18:11:18 -0400

    Modifying react app

--- ontheroad_ui/src/App.js
+++ ontheroad_ui/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React-002
+          Learn React-003
         </a>
       </header>
     </div>
