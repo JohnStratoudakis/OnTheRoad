e4cb8e231583c2871bfb357a1311f5f03357a8cd
John Stratoudakis <johnstratoudakis@gmail.com>
2019-10-24 19:23:00 -0400

    Switch

--- ontheroad_ui/src/App.js
+++ ontheroad_ui/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React-003
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {value: 'coconut'};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        console.log('handleChange(event)');
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        console.log("Handle Submit Event");
+        console.log("this.state: {this.state}");
+        console.log("event: {typeof(event)}");
+        event.preventDefault();
+        //this.out.value = "DDDD";
+    }
+
+    render() {
+        var placeholder = "Amsterdam, The Netherlands\nBrussels, Belgium\nLondon, England";
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Euro Trip Calculator</label>
+                        <br />
+                        <textarea placeholder={placeholder}
+                                  onBlur={this.handleChange.bind(this)}
+                                  rows='8'
+                                  cols='30'
+                            ></textarea>
+                        <input type="submit" value="Submit" />
+                    </form>
+                    Recommended Trip Order:
+                    <br />
+                    {this.state.value}
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
