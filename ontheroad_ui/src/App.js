a428f9fc0d90e040907d1112dd64c7faf86fa657
John Stratoudakis <johnstratoudakis@gmail.com>
2019-11-05 08:33:05 -0500

    Wiring up almost done

--- ontheroad_ui/src/App.js
+++ ontheroad_ui/src/App.js
@@ -15,35 +15,75 @@ class App extends React.Component {
     }
 
     handleChange(event) {
-        console.log('handleChange(event)');
-        this.setState({results: event.target.value});
+//        console.log('handleChange(event)');
+//        console.log("this.state: " + this.state);
+//        console.log("this.state['results']: " + this.state['results']);
+//        console.log("this.state: " + typeof(this.state));
+//        console.log(`typeof(this.state): ${typeof(this.state.toString())}`);
+//        console.log("=======");
+        var addr_array = [];
+
+        var addresses = event.target.value.split('\n');
+        addresses.forEach(function(val,index,array){
+            var name = array[index].split(',')[0];
+            var address = array[index];
+            var new_location = [name, address];
+            addr_array.push( new_location );
+            console.log(`Address[${index}]: ` + new_location);
+        });
+        this.setState({results: addr_array});
     }
 
     handleSubmit(event) {
-        console.log("Handle Submit Event");
+        console.log("handleSubmit()...");
         console.log(`this.state: ${this.state}`);
         console.log(`event: ${typeof(event)}`);
 
+        //console.log("this.state: " + this.state);
+        //console.log("this.state: " + typeof(this.state));
+        //console.log(`typeof(this.state): ${typeof(this.state.toString())}`);
         // TODO: Read this from config or env var
         var serverHost = "127.0.0.1";
-        console.log(`Server Host Name: ${serverHost}`);
+        //console.log(`Server Host Name: ${serverHost}`);
 
         // TODO: Read this from form
+        var results = this.state['results'];
         var request = {
-            "locations": [
-                ["Amsterdam", "Amsterdam, The Netherlands"],
-                ["London", "London, England"],
-                ["Brussels", "Brussels, Belgium"]
-            ]
+            "locations": results
+//                ["Amsterdam", "Amsterdam, The Netherlands"],
+//                ["London", "London, England"],
+//                ["Brussels", "Brussels, Belgium"]
+//            ]
         };
-        axios.post(`http://127.0.0.1:5000/onTheRoad`, request )
+
+        console.log("RESULTS: " + results);
+        console.log("RESULTS: " + results.length);
+        //var addresses = results.split('\n');
+        /*
+        addresses.forEach(function(val,index,array){
+            var name = array[index].split(',')[0];
+            var address = array[index];
+            var new_location = new Array(name, address);
+            //addr_array.push( new_location );
+            console.log(`Address[${index}]: ` + new_location);
+        });
+*/
+        console.log("this.state['results']: " + this.state['results']);
+//        request['locations'].push( ["Amsterdam", "--------Amsterdam, The Netherlands"] );
+        console.log("Sending the following request: " + request);
+        console.log(`Sending the following request ${request}`);
+        console.log(request);
+
+        axios.post(`http://${serverHost}:5000/onTheRoad`, request )
               .then(res => {
                       console.log(res);
                               console.log(res.data);
                                     });
 
-        var results = "Results of TSP";
+        var results_text = "Results of TSP";
+        console.log("RESULTS OF TSP: " + res);
         this.setState({results: results});
+
         event.preventDefault();
     }
 
