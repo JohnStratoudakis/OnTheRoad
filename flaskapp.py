405857b8f2a5ea751a31f2ea9a703d1a280ddbd5
John Stratoudakis <johnstratoudakis@gmail.com>
2019-11-06 08:53:24 -0500

    bump

--- flaskapp.py
+++ flaskapp.py
@@ -5,19 +5,33 @@ from flask import request
 
 from flask import jsonify
 
+import logging
+
 from OnTheRoad import BestPath, Location, TravelCost
+
+LINE_LENGTH = 90
+
+logger = logging.getLogger(__name__.split('.')[0])
 
 app = Flask(__name__)
 
+#def set_up_logging():
+#    logger.setLevel(logging.DEBUG)
+#    formatter = logging.Formatter(f'%(asctime)s - %(message)s')
+#
+#    log_file_path = './ontheroad_ui.log'
+#    file_stream_handler = logging.FileHandler(log_file_path)
+#    file_stream_handler.setLevel(logging.DEBUG)
+
 @app.route('/onTheRoad', methods=['POST'])
 def post():
-    print("----------------------------------------")
+    print("=" * LINE_LENGTH)
     if request.is_json:
         content = request.get_json()
         # TODO: Use python logger with appropriate log leves
         #print(f"Content: {content}")
         locations = content['locations']
-        #print(f"Locations: {locations}")
+        print(f"Locations: {locations}")
 
         allCities = []
         for location in locations:
