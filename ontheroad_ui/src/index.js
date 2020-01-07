eb14dab6ae862e7df690efbde169623404106829
John Stratoudakis <johnstratoudakis@gmail.com>
2020-01-07 09:13:47 -0500

    remove comment

--- ontheroad_ui/src/index.js
+++ ontheroad_ui/src/index.js
@@ -8,7 +8,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//ReactDOM.render(<ThemeSwitcher />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
