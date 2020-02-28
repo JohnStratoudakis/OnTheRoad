e35820de165d6902b3e4848d29072947db4342c8
John Stratoudakis <johnstratoudakis@gmail.com>
2020-02-28 09:34:24 -0500

    Almost there...

--- ontheroad_ui/src/App.js
+++ ontheroad_ui/src/App.js
@@ -3,8 +3,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 
-import { hello_world, sum } from './helpers';
-//import axios from 'axios';
+import axios from 'axios';
 
 import Location from './Location.js';
 
@@ -52,20 +51,33 @@ class App extends React.Component {
     serverHost = process.env.REACT_APP_HOST_IP;
     serverPort = "5000";
 
-//    handleChange(event) {
-//        var addr_array = [];
-//
-//        var addresses = event.target.value.split('\n');
-//        addresses.forEach(function(val,index,array){
-//            var name = array[index].split(',')[0];
-//            var address = array[index];
-//            var new_location = [name, address];
-//            addr_array.push( new_location );
-//            console.log(`Adding address[${index}]: ` + new_location);
-//        });
-//        this.setState({results: addr_array});
-//    }
-//
+    constructor(props) {
+        super(props);
+        //this.dump_test = this.dump_test.bind(this);
+        this.state = {
+          results: 'res',
+          addresses: [],
+          guests: [
+              {name:'Paris', address:'Paris, France'},
+              {name:'Amsterdam', address:'Amsterdam, The Netherlands'},
+              {name:'Brussels', address:'Brussels, Belgium'},
+          ]
+        };
+        this.handleCalculate = this.handleCalculate.bind(this);
+        this.handleOnAddressesChange = this.handleOnAddressesChange.bind(this);
+    }
+
+    dump_test(addresses) {
+      console.log(`addresses.length: ${addresses.length}`);
+      for(var i=0; i < addresses.length; i++) {
+        var element = addresses[i];
+        console.log(`element.length: ${element.length}`)
+        for(var j=0; j < element.length; j++) {
+          console.log(`addresses[${i}][${j}]: ${addresses[i][j]}`);
+        }
+      }
+    }
+
 //    handleSubmit(event) {
 //        console.log("handleSubmit()...");
 //        console.log(`this.state: ${this.state}`);
@@ -76,7 +88,6 @@ class App extends React.Component {
 //        var request = {
 //            "locations": results
 //        };
-
 //        console.log("RESULTS: " + results);
 //        console.log("RESULTS: " + results.length);
 //        //var addresses = results.split('\n');
@@ -112,55 +123,84 @@ class App extends React.Component {
 //
 //        event.preventDefault();
 //    }
-//
-//    getPlace(address, placeID) {
-//        this.saveAddress(address, placeID)
-//        geocodeByAddress(address)
-//          .then(results => getLatLng(results[0]) )
-//          .then(latlng => this.saveLocation(latlng) )
-//          .catch(error => console.error('Error', error))
-//    }
-    constructor(props) {
-        super(props);
-        //this.dump_test = this.dump_test.bind(this);
-        this.state = {
-          results: 'res',
-          addresses: [],
-          guests: [
-              {name:'Paris', address:'Paris, France'},
-              {name:'Amsterdam', address:'Amsterdam, The Netherlands'},
-              {name:'Brussels', address:'Brussels, Belgium'},
-          ]
-        };
-        this.handleCalculate = this.handleCalculate.bind(this);
-        this.handleOnAddressesChange = this.handleOnAddressesChange.bind(this);
-    }
-
-    dump_test(addresses) {
-      console.log(`addresses.length: ${addresses.length}`);
-      for(var i=0; i < addresses.length; i++) {
-        var element = addresses[i];
-        console.log(`element.length: ${element.length}`)
-        for(var j=0; j < element.length; j++) {
-          console.log(`addresses[${i}][${j}]: ${addresses[i][j]}`);
-        }
-      }
-    }
-
     handleCalculate(event) {
         console.log("handleCalculate()");
-        //console.log(`${event.toString()}`);
+//        console.log(`${event.toString()}`);
 
-//        this.dump_test(this.Location.state.allAddresses);
-      var addresses = this.state.addresses;
-      console.log(`addresses.length: ${addresses.length}`);
-      for(var i=0; i < addresses.length; i++) {
-        var element = addresses[i];
-        console.log(`element.length: ${element.length}`)
-        for(var j=0; j < element.length; j++) {
-          console.log(`addresses[${i}][${j}]: ${addresses[i][j]}`);
+        var request_addresses = [];
+        var addresses = this.state.addresses;
+        console.log(`addresses.length: ${addresses.length}`);
+        for(var i=0; i < addresses.length; i++) {
+            var element = addresses[i];
+            console.log(`addresses[${i}][2]: ${addresses[i][2]}`);
+            request_addresses.push(addresses[i][2]);
+//            console.log(`element.length: ${element.length}`)
+//            for(var j=0; j < element.length; j++) {
+//                console.log(`addresses[${i}][${j}]: ${addresses[i][j]}`);
+//            }
         }
-      }
+
+        // Now create request
+        request_addresses = [];
+        request_addresses.push(["Paris", "Paris, France"])
+        request_addresses.push(["Amsterdam", "Amsterdam, The Netherlands"])
+        var request = {
+            "locations": request_addresses
+        };
+        console.log("REQUEST: " + request.locations);
+
+        // Make request
+        //axios.post(`https://${this.serverHost}:${this.serverPort}/onTheRoad`, request )
+        axios.post(`https://${this.serverHost}/onTheRoad`, request )
+            .then(res => {
+                console.log("DUMPING res");
+                console.log(res);
+                console.log("DUMPING res.data");
+                console.log(res.data);
+                console.log("DUMPING res.data.best_path");
+                console.log(res.data['best_path']);
+                var results_text = "Results of TSP";
+                results_text = res.data['best_path'];
+                console.log("RESULTS OF TSP: " + res);
+                this.setState({results: results_text});
+            });
+
+        event.preventDefault();
+
+        // Render Response
+//        console.log("RESULTS: " + results.length);
+//        //var addresses = results.split('\n');
+//        /*
+//        addresses.forEach(function(val,index,array){
+//            var name = array[index].split(',')[0];
+//            var address = array[index];
+//            var new_location = new Array(name, address);
+//            //addr_array.push( new_location );
+//            console.log(`Address[${index}]: ` + new_location);
+//        });
+//*/
+//        console.log("this.state['results']: " + this.state['results']);
+////        request['locations'].push( ["Amsterdam", "--------Amsterdam, The Netherlands"] );
+//        console.log("Sending the following request: " + request);
+//        console.log(`Sending the following request ${request}`);
+//        console.log(request);
+//
+//        //axios.post(`https://${this.serverHost}:${this.serverPort}/onTheRoad`, request )
+//        axios.post(`https://${this.serverHost}/onTheRoad`, request )
+//            .then(res => {
+//                console.log("DUMPING res");
+//                console.log(res);
+//                console.log("DUMPING res.data");
+//                console.log(res.data);
+//                console.log("DUMPING res.data.best_path");
+//                console.log(res.data['best_path']);
+//                var results_text = "Results of TSP";
+//                results_text = res.data['best_path'];
+//                //console.log("RESULTS OF TSP: " + res);
+//                this.setState({results: results_text});
+//            });
+//
+//        event.preventDefault();
     }
 
     handleOnAddressesChange(new_addresses) {
@@ -173,11 +213,7 @@ class App extends React.Component {
                 console.log(`new_addresses[${i}][${j}]: ${new_addresses[i][j]}`);
             }
         }
-        //this.handleOAddressesChange = this.handleOnAddressesChange.bind(this);
         this.setState({addresses: new_addresses});
-        hello_world();
-        sum();
-        //this.dump_test(new_addresses);
     }
 
     render() {
