b7bdbc69376d3a8590d7cf6b419fe6cbab80517f
John Stratoudakis <johnstratoudakis@gmail.com>
2020-03-14 11:27:09 -0400

    DEBUG

--- ontheroad_ui/src/App.js
+++ ontheroad_ui/src/App.js
@@ -212,7 +212,7 @@ class App extends React.Component {
                     <br />
                     <div style={{ position: 'relative', width: '100%'}}>
                     <h5>Location</h5>
-                    <h3>Local Storage DEBUG_8 INFO:</h3>
+                    <h3>Local Storage DEBUG_9 INFO:</h3>
                     <div>
                     {
                         addresses && addresses.map((city, idx) => 
