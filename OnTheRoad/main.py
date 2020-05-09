d0a704686f2751d01e532785cfd67e79fbb20751
John Stratoudakis <johnstratoudakis@gmail.com>
2020-05-09 15:02:33 -0400

    Bump to 0.0.5

--- OnTheRoad/main.py
+++ OnTheRoad/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/version', methods=['GET'])
 def get_version():
     print("get_version() called")
-    version = "0.0.4"
+    version = "0.0.5"
     print(f"Returning version: {version}")
 
     response_raw_text = f"OnTheRoad WSGI Version: {version}\n"
