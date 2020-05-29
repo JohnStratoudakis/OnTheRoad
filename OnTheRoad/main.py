29e2dcbf5f17636f76db65b5c4c4b18a4356beee
John Stratoudakis <johnstratoudakis@gmail.com>
2020-05-29 16:48:39 -0400

    Disable CORS, have to reproduce locally first

--- OnTheRoad/main.py
+++ OnTheRoad/main.py
@@ -1,14 +1,14 @@
 
 from flask import Flask
-from flask_cors import CORS
+#from flask_cors import CORS
 
 from OnTheRoad import flask_config
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 @app.route('/version', methods=['GET'])
-@crossdomain(origin='*')
+#@crossdomain(origin='*')
 def get_version():
     version = flask_config.version
     print(f"Returning version: {version}")
