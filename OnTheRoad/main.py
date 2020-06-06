d0dfa3e0ffcfeb8f166e1c2209b0ce570e974bd7
John Stratoudakis <johnstratoudakis@gmail.com>
2020-06-06 09:42:19 -0400

    Testing - old ver to new

--- OnTheRoad/main.py
+++ OnTheRoad/main.py
@@ -17,9 +17,6 @@ cors = CORS(app, resources={r"/": {"origins": "johnstratoudakis.com"}})
 #@flask_cors.cross_origin()
 def get_version():
     version = flask_config.version
-    #version="1.0.1"
-    print(f"Returning version: {version}")
-    print(f"Repeating myself: Returning version: {version}")
 
     response_raw_text = f"OnTheRoad WSGI Version: {version}\n"
 
