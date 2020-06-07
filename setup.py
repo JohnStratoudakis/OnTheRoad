a02fab212ada6d267bed5d24fc6a207d4ef00302
Bumpversion after CI <johnstratoudakis@egmail.com>
2020-06-07 21:00:28 +0000

    Bump version: 0.0.134 -> 0.0.135 [ci skip]

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="OnTheRoad",
-    version="0.0.134",
+    version="0.0.135",
     author="John Stratoudakis",
     author_email="johnstratoudakis@gmail.com",
     license="",
