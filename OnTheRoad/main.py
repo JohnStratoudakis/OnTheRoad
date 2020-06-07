80302384ff0ded14b5792668af60ff73665342b7
John Stratoudakis <johnstratoudakis@gmail.com>
2020-06-07 18:17:47 -0400

    Using example from flask homepage

--- OnTheRoad/main.py
+++ OnTheRoad/main.py
@@ -9,9 +9,26 @@ import logging
 import sys
 from OnTheRoad import flask_config
 
-logger = logging.getLogger(__name__.split('.')[0])
-logger.addHandler(default_handler)
-logger.addHandler(logging.StreamHandler(sys.stdout))
+from logging.config import dictConfig
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+#logger = logging.getLogger(__name__.split('.')[0])
+#logger.addHandler(default_handler)
+#logger.addHandler(logging.StreamHandler(sys.stdout))
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/": {"origins": "johnstratoudakis.com"}})
